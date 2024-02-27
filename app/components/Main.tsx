62562f9d56ae45070b312e37c8ece17f2812fa21
Harshnaruto7 <levitechnology600@gmail.com>
2024-02-27 10:17:10 +0530

    second commit

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -5,32 +5,6 @@ export default function Main() {
   return (
     <div>
 
-      {/* <div className="flex flex-col w-full lg:flex-row">
-        <div className="grid flex-grow h-32 card rounded-box place-items-center">
-          <Navbar />
-          <div className=" grid justify-items-center... ">
-        
-          </div>
-          
-        </div>
-        <div className="divider lg:divider-horizontal">OR</div> 
-      
-        <div className="grid flex-grow h-32 card rounded-box place-items-center ">
-
-         
-          <div className=" flex flex-col grow gap-10 mt-20 ">
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <br></br>
-          </div>
-     
-
-
-
-
-        </div>
-      </div> */}
 
 
 <div className="flex flex-col lg:flex-row w-full min-h-screen">
@@ -38,9 +12,31 @@ export default function Main() {
     <div className="w-full text-center py-4 sticky top-0 bg-white z-10">
       <Navbar />
       <div className=" flex flex-col  gap-5 mt-5">
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+          <Card  
+
+              title="$72.10/week" 
+              description="Mining:0xafoe502..." 
+              description2="Minimum payout:$82.27" 
+              buttonText="Listen"
+              showIcons={true} // Correct prop usage
+              value="30"
+            />
+            <Card 
+              title="$72.10/week" 
+              description="Mining:0xafoe502..." 
+              description2="Minimum payout:$82.27" 
+              buttonText="Explore"  
+              value="70"
+              showIcons2={true}
+            />
+            <Card 
+              title="$72.10/week" 
+              description="Mining:0xafoe502..." 
+              description2="Minimum payout:$82.27" 
+              buttonText="Dive In"
+              value="90" 
+              showIcons3={true}
+            />
 
       </div>
     </div>
@@ -51,9 +47,30 @@ export default function Main() {
   <div className="flex flex-col w-full lg:w-1/2 h-full overflow-auto">
     {/* Adjustments for right side content for better mobile view */}
     <div className="flex flex-col items-center justify-center flex-grow space-y-4 lg:space-y-10 mt-4 lg:mt-20 p-4 lg:p-0 overflow-auto">
-      <Card />
-      <Card />
-      <Card />
+    <Card  
+              title="$72.10/week" 
+              description="Mining:0xafoe502..." 
+              description2="Minimum payout:$82.27" 
+              buttonText="Listen"
+              showIcons={true} // Correct prop usage
+              value="30"
+            />
+            <Card 
+              title="$72.10/week" 
+              description="Mining:0xafoe502..." 
+              description2="Minimum payout:$82.27" 
+              buttonText="Explore"  
+              value="70"
+              showIcons2={true}
+            />
+            <Card 
+              title="$72.10/week" 
+              description="Mining:0xafoe502..." 
+              description2="Minimum payout:$82.27" 
+              buttonText="Dive In"
+              value="90" 
+              showIcons3={true}
+            />
     </div>
   </div>
 </div>
